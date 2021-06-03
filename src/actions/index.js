c441ec4d96b0d870258c86981c29f5dfec572796
emmac124 <emma.cooperhawkins@gmail.com>
2021-06-02 17:01:00 -0700

    added new character

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const CHARACTERS_SUCCESS = "CHARACTERS_SUCCESS";
 export const fetchAllCharacters = () => {
     return (dispatch) => {
         axios
-            .get("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,8,9,11,13,14,15")
+            .get("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,17,9,11,13,14,15")
             .then(res => {
                 console.log("Results", res);
                 dispatch({type: CHARACTERS_SUCCESS, payload: res.data})
